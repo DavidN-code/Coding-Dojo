04df996578cd5afcd5080c199226095d8a32855d
DavidN-code <130241830+DavidN-code@users.noreply.github.com>
2023-04-30 06:04:53 -0500

    Delete ProgrammingBasics directory

--- ProgrammingBasics/parameterPractice.js
+++
@@ -1,8 +0,0 @@
-function greet(name,time){
-    console.log("Good day,", name+"!");
-    console.log("It's",time,"o'clock")
-    if (name == "Count Dooku"){
-        console.log("I'm coming for you, Dooku!")
-    }
-}
-greet("Count Dooku",12)
