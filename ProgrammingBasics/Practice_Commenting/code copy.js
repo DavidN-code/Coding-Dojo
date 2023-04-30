dff22da3b90b46e803f8ab4858d8b2258954ce09
DavidN-code <130241830+DavidN-code@users.noreply.github.com>
2023-04-30 08:50:18 -0500

    Add files via upload

---
+++ ProgrammingBasics/Practice_Commenting/code copy.js
@@ -0,0 +1,9 @@
+// This likeCount variable will be a number set to zero
+var likeCount = 0;
+// This function code block will increase the likeCount variable's value by 1
+function increaseLikes() {
+    /* This operation increases likeCount by 1.  
+    Every time the increaseLikes function is used, the likeCount variable is increased by 1
+    */
+    likeCount = likeCount + 1;
+}
